b0223e1b7d7ae32633945180a6383172f895b410
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-02-04 18:25:47 -0300

    feat: script db

--- backend/scripts.sql
+++ backend/scripts.sql
@@ -1,16 +1,10 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-DO $$ BEGIN IF NOT EXISTS (
-    SELECT 1
-    FROM information_schema.tables
-    WHERE table_name = 'clients'
-) THEN CREATE TABLE clients (
+CREATE TABLE IF NOT EXISTS  clients (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255),
     email VARCHAR(150),
     phone VARCHAR(20),
     coordinate_x INTEGER,
     coordinate_y INTEGER
-);
-END IF;
-END $$;
+)
