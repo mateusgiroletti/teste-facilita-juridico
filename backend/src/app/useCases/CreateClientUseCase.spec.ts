9a51159c61271e6b2d60954dd4a961e7f5250ada
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-02-05 00:08:51 -0300

    test

--- backend/src/app/useCases/CreateClientUseCase.spec.ts
+++ backend/src/app/useCases/CreateClientUseCase.spec.ts
@@ -10,7 +10,6 @@ describe("CreateClientUseCase", () => {
     let mockClientRepository: jest.Mocked<ClientRepository>;
 
     beforeEach(() => {
-        // Limpa o mock entre os testes
         jest.clearAllMocks();
         const mockPostgreeClient = {} as PostgreeClient;
 
@@ -19,25 +18,20 @@ describe("CreateClientUseCase", () => {
     });
 
     it("should be create a new client and return the ID", async () => {
-        // Configuração do mock para simular a criação bem-sucedida de um cliente
         const mockCreateClientDto: CreateClientDTO = {
-            name: "Novo Cliente",
+            name: "client",
             email: "email@email.com",
             phone: 666666,
             coordinate_x: 36,
             coordinate_y: 50
         };
 
-        const mockClientId = 1; // O ID simulado retornado pelo banco de dados
+        const mockClientId = 1;
         mockClientRepository.create.mockResolvedValue(mockClientId);
 
-        // Executa o caso de uso
         const result = await createClientUseCase.execute(mockCreateClientDto);
 
-        // Verifica se o método create foi chamado com os argumentos corretos
         expect(mockClientRepository.create).toHaveBeenCalledWith(mockCreateClientDto);
-
-        // Verifica se o resultado é esperado
         expect(result.id).toBe(mockClientId);
     });
 });
