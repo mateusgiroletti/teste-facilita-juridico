ad3e4499626ce53bcfbbea5139e14e2cd1a5d299
Mateus Giroletti <mateus.giroletti97@gmail.com>
2024-02-05 10:28:59 -0300

    test

--- backend/src/app/useCases/CalculateDistanceUseCase.spec.ts
+++ backend/src/app/useCases/CalculateDistanceUseCase.spec.ts
@@ -8,8 +8,8 @@ describe("CalculateDistanceUseCase", () => {
     });
 
     it("Shold be calculate the correct distance between two points", () => {
-        const point1 = { x: 0, y: 0 };
-        const point2 = { x: 3, y: 4 };
+        const point1 = { coordinate_x: 0, coordinate_y: 0 };
+        const point2 = { coordinate_x: 3, coordinate_y: 4 };
 
         const expectedDistance = 5;
 
@@ -19,13 +19,13 @@ describe("CalculateDistanceUseCase", () => {
     });
 
     it("should fail to calculate incorrect distance between two points", () => {
-        const point1 = { x: 0, y: 0 };
-        const point2 = { x: 3, y: 4 };
-    
+        const point1 = { coordinate_x: 0, coordinate_y: 0 };
+        const point2 = { coordinate_x: 3, coordinate_y: 4 };
+
         const expectedDistance = 10;
-    
+
         const calculatedDistance = calculateDistanceUseCase.execute(point1, point2);
-    
+
         expect(calculatedDistance).not.toBe(expectedDistance);
     });
 });
